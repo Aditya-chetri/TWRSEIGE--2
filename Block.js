05dd81cec759a5b392d3f7bbe0cae4da9e63f1ca
Aditya-chetri <64538450+Aditya-chetri@users.noreply.github.com>
2020-07-21 20:18:32 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -16,7 +16,7 @@ class Block{
       }
      
       display(){
-        if(this.body.speed <3){
+        if(this.body.speed <6){
         var angle = this.body.angle;
         var pos= this.body.position;
         push();
